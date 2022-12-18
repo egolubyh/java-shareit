cc9e4727217620c872fc0b57c4a6bcfa96b1d1a9
egolubyh <egolubyh@yandex.ru>
2022-12-18 10:44:25 +0500

    add ItemRepository

---
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareit.item.repository;
+
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+
+public interface ItemRepository {
+    Item saveItem(Item item);
+    Item findItemById(Long itemId);
+    List<Item> findItemByOwner(Long ownerId);
+    List<Item> findItemByParam(String param);
+}

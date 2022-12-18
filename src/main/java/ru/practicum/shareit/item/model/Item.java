72216fd96f25d1f894980c7b67e34413bafff313
egolubyh <egolubyh@yandex.ru>
2022-12-18 10:42:54 +0500

    add Item.java

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,39 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
 public class Item {
+    /**
+     * уникальный идентификатор вещи
+     */
+    private Long id;
+    /**
+     * краткое название
+     */
+    private String name;
+    /**
+     * развёрнутое описание
+     */
+    private String description;
+    /**
+     * статус о том, доступна или нет вещь для аренды
+     */
+    private Boolean available;
+    /**
+     * владелец вещи
+     */
+    private User owner;
+    /**
+     * если вещь была создана по запросу другого пользователя, то в этом
+     * поле будет храниться ссылка на соответствующий запрос.
+     */
+    private ItemRequest request;
 }

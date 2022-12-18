f0e63d8e966434d15d7e35f3cef5699864599317
egolubyh <egolubyh@yandex.ru>
2022-12-18 10:42:11 +0500

    add Exceptions

---
+++ src/main/java/ru/practicum/shareit/item/exception/NoDescriptionException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.item.exception;
+
+public class NoDescriptionException extends Exception {
+    public NoDescriptionException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/item/exception/NoNameException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.item.exception;
+
+public class NoNameException extends Exception {
+    public NoNameException(String message) {
+        super(message);
+    }
+}
